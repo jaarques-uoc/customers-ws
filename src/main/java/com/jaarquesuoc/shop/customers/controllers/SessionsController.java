e668f8486e16836f3edf0152f351a1838ff24086
Jordi <jaarques@uoc.edu>
2019-05-19 21:40:37 +0200

    Formatting

--- src/main/java/com/jaarquesuoc/shop/customers/controllers/SessionsController.java
+++ src/main/java/com/jaarquesuoc/shop/customers/controllers/SessionsController.java
@@ -4,11 +4,12 @@ import com.jaarquesuoc.shop.customers.dtos.CustomerDto;
 import com.jaarquesuoc.shop.customers.services.SessionsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -19,12 +20,12 @@ public class SessionsController {
     @PostMapping("/login")
     public CustomerDto login(@RequestBody final CustomerDto customerDto) {
         return sessionsService.login(customerDto)
-            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+            .orElseThrow(() -> new ResponseStatusException(NOT_FOUND));
     }
 
     @PostMapping("/signup")
     public CustomerDto signup(@RequestBody final CustomerDto customerDto) {
         return sessionsService.customerSignup(customerDto)
-            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+            .orElseThrow(() -> new ResponseStatusException(NOT_FOUND));
     }
 }
