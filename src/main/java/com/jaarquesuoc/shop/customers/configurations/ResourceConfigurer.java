a57af566a68024762808c95e9b6578ac6aa0bb88
Jordi <jaarques@uoc.edu>
2019-05-26 22:41:56 +0200

    Trying to make it work

---
+++ src/main/java/com/jaarquesuoc/shop/customers/configurations/ResourceConfigurer.java
@@ -0,0 +1,12 @@
+package com.jaarquesuoc.shop.customers.configurations;
+
+//@Configuration
+//public class ResourceConfigurer {}extends ResourceServerConfigurerAdapter {
+//
+//    @Override
+//    public void configure(final HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//            .antMatchers("/**/health").permitAll()
+//            .antMatchers("/**").authenticated();
+//    }
+//}
